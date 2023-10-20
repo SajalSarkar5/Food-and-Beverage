d45a5d2ee73ce20d3a80e9feec293d1a59ab44fc
SajalSarkar5 <sarkarsajal810@gmail.com>
2023-10-20 23:59:04 +0600

    added private route

--- src/pages/SameBrandDetails.jsx
+++ src/pages/SameBrandDetails.jsx
@@ -35,9 +35,7 @@ const SameBrandDetails = () => {
 
                                     <div>
 
-
-
-                                    </div>
+                                </div>
 
                                 </div>
 
